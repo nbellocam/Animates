db8248d39d3cddbb9b948a97d910ac7c8d6e472c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-08-31 18:27:11 -0300

    Create based structure for the website

--- code/server/routes/index.js
+++ code/server/routes/index.js
@@ -3,5 +3,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+};
+
+exports.canvas = function(req, res){
+  res.render('canvas', { title: 'canvas' });
 };
