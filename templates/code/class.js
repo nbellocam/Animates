5fc621de0d49ace32c2faa0c06c85265cb5e4ae8
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-10-06 20:11:17 -0300

    updated class template

--- templates/code/class.js
+++ templates/code/class.js
@@ -1,40 +1,49 @@
-/**
- *	Creates a new ClassName and i'm documenting it with jsdoc.
- *  @class Represents a ClassName . 
- */ 
-function TemplateClass (options) 
-{
-	var $this = this,		// Save the this reference for later use
-		privateAttribute = 'Yeah baby im private';
+/*global window, fabric $ */
 
+var namespace = namespace || {};
+
+(function (ns){
 
 	/**
-	 * A public function asigned to the  current instance this.
-	 * @param {integer} param1 The description of the param1.
-	 * @param {string} [param2] The description of the optional param2.
-	 */
-	this.publicMethodPrint = function publicMethodSample(param1, param2) 
+	 *	Creates a new ClassName and i'm documenting it with jsdoc.
+	 *  @class Represents a ClassName . 
+	 */ 
+	var TemplateClass = function (options) 
 	{
-		console.log('this a public function accessing a private attribute value "' + privateAttribute + '"');
-	};
+		var $this = this,		// Save the this reference for later use
+			privateAttribute = 'Yeah baby im private';
 
-	/**
-	 * a private function in the closure of the current instace this.
-	 * @param {integer} param1 The description of the param1.
-	 */
-	function privateMethodPrint (param1)
-	{
-		console.log('you cannot call me from the outside');
-	}
 
-	(function init() {
-		console.log('im kinda constructor');
-		privateMethodPrint();
-	})();
+		/**
+		 * A public function asigned to the  current instance this.
+		 * @param {integer} param1 The description of the param1.
+		 * @param {string} [param2] The description of the optional param2.
+		 */
+		this.publicMethodPrint = function publicMethodSample(param1, param2) 
+		{
+			console.log('this a public function accessing a private attribute value "' + privateAttribute + '"');
+		};
 
-};
+		/**
+		 * a private function in the closure of the current instace this.
+		 * @param {integer} param1 The description of the param1.
+		 */
+		function privateMethodPrint (param1)
+		{
+			console.log('you cannot call me from the outside');
+		}
 
-var newObject = new TemplateClass({});
+		(function init() {
+			console.log('im kinda constructor');
+			privateMethodPrint();
+		})();
+
+	};	
+	ns.TemplateClass = TemplateClass;
+
+})(namespace);
+
+var newObject = new namespace.TemplateClass({});
 newObject.publicMethodPrint(1, 'test');
 
 if (newObject.privateMethodPrint)
