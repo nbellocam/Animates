162530da56c9167f03c63ff07d51ce5c99159e36
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-10-06 23:37:14 -0300

    Added basic structure for model clases

---
+++ code/model-client/js/animates/ShapeFrame.js
@@ -0,0 +1,45 @@
+/*global window, fabric, $ */
+
+var Animates = Animates || {};
+
+(function (ns){
+
+	/**
+	 *	Creates a new ClassName and i'm documenting it with jsdoc.
+	 *  @class Represents a ClassName . 
+	 */ 
+	var ShapeFrame = function (options) 
+	{
+		var $this = this,		// Save the this reference for later use
+			privateAttribute = 'Yeah baby im private';
+
+
+		/**
+		 * A public function asigned to the  current instance this.
+		 * @param {integer} param1 The description of the param1.
+		 * @param {string} [param2] The description of the optional param2.
+		 */
+		this.publicMethodPrint = function publicMethodSample(param1, param2) 
+		{
+			console.log('this a public function accessing a private attribute value "' + privateAttribute + '"');
+		};
+
+		/**
+		 * a private function in the closure of the current instace this.
+		 * @param {integer} param1 The description of the param1.
+		 */
+		function privateMethodPrint (param1)
+		{
+			console.log('you cannot call me from the outside');
+		}
+
+		(function init() {
+			console.log('im kinda constructor');
+			privateMethodPrint();
+		})();
+
+	};	
+	ns.ShapeFrame = ShapeFrame;
+
+})(Animates);
+
