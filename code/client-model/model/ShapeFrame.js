2300e29677f1b6ec4ff7261245c965c67198a551
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-10-08 18:48:50 -0300

    Added client-model project

---
+++ code/client-model/model/ShapeFrame.js
@@ -0,0 +1,27 @@
+/*global window, fabric, $ */
+
+var Animates = Animates || {};
+
+(function (ns){
+
+	/**
+	 *	Creates a new ShapeFrame.
+	 *  @class Represents a ShapeFrame . 
+	 */ 
+	var ShapeFrame = function (options) 
+	{
+		var $this = this, // Save the this reference for later use
+
+
+		/**
+		 *	Constructor
+		 */ 
+		(function init() {
+		})();
+
+	};	
+
+	ns.ShapeFrame = ShapeFrame;
+
+})(Animates);
+
