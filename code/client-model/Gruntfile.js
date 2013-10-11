0f5167a31b1d2424337be098b9404b877c1cd6c2
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-10-11 18:54:42 -0300

    first version

--- code/client-model/Gruntfile.js
+++ code/client-model/Gruntfile.js
@@ -11,31 +11,71 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		jshint: {
-			files: ['**/*.js'],
+			all: ['**/*.js'],
+			model: ['model/**/*.js'],
+			tests: ['test/**/*.js'],
 			options: {
-				ignores: ['node_modules/**/*.js'],
+				ignores: ['node_modules/**/*.js', 'coverage/**/*.js'],
 				globals: {
 					module: true
 				}
 			}
 		},
 		watch: {
-			jshint: {
-				files: jshint.files,
-				tasks: ['jshint']
+			model: {
+				files: '<%= jshint.model.src %>',
+				tasks: ['jshint:model', 'mochaTest']
 			},
-			//test: {
-			//	files: '<%= jshint.test.src %>',
-			//	tasks: ['jshint:test', 'qunit']
-			//},
+			test: {
+				files: '<%= jshint.test.src %>',
+				tasks: ['jshint:tests', 'mochaTest']
+			},
+			all: {
+				files: '<%= jshint.all.src %>',
+				tasks: ['jshint:all', 'mochaTest']
+			}
+		},
+		// Configure a mochaTest task
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					// Require blanket wrapper here to instrument other required
+					// files on the fly. 
+					//
+					// NB. We cannot require blanket directly as it
+					// detects that we are not running mocha cli and loads differently.
+					//
+					// NNB. As mocha is 'clever' enough to only run the tests once for
+					// each file the following coverage task does not actually run any
+					// tests which is why the coverage instrumentation has to be done here
+					require: 'coverage/blanket'
+				},
+				src: ['test/**/*.js']
+			},
+			coverage: {
+				options: {
+					reporter: 'html-cov',
+					// use the quiet flag to suppress the mocha console output
+					quiet: true,
+					// specify a destination file to capture the mocha
+					// output (the quiet option does not suppress this)
+					captureFile: 'coverage.html'
+				},
+				src: ['test/**/*.js']
+			}
 		}
 	});
 
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Add the grunt-mocha-test tasks.
+	grunt.loadNpmTasks('grunt-mocha-test');
 
-	grunt.registerTask('test', ['jshint']);
+
+	grunt.registerTask('test', ['jshint:all','mochaTest']);
+
 	// Default task
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint:all']);
 };
