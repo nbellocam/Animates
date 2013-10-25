e4d87139516b29a02d68a774368f8b687a3fa9fb
Pablo Biasotti <pbiasotti@hotmail.com>
2013-10-24 23:53:42 -0300

    added the new Gruntfiles with standar tasks

--- code/client-model/Gruntfile.js
+++ code/client-model/Gruntfile.js
@@ -9,101 +9,186 @@ module.exports = function (grunt) {
 
 		//Read the package.json (optional)
 		pkg: grunt.file.readJSON('package.json'),
-
-		jsdoc : {
-			dist : {
-				src: ['src/**/*.js'], 
-				dest: 'build/output/doc'
-			}
-		},
-		jshint: {
-			all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-			code: ['src/**/*.js'],
-			tests: ['test/**/*.js'],
-			options: {
-				expr: true,
-				ignores: ['node_modules/**/*.js'],
-				globals: {
-					module: true
-				}
-			}
-		},
-		watch: {
-			code: {
-				files: '<%= jshint.code %>',
-				tasks: ['jshint:code', 'mochaTest']
-			},
-			test: {
-				files: '<%= jshint.tests %>',
-				tasks: ['jshint:tests', 'mochaTest']
-			},
-			all: {
-				files: '<%= jshint.all %>',
-				tasks: ['jshint:all', 'mochaTest']
-			},
-			runTests: {
-				files: '<%= jshint.all %>',
-				tasks: ['mochaTest']
-			}
-		},
-		// Configure a mochaTest task
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec',
-					// Require blanket wrapper here to instrument other required
-					// files on the fly. 
-					//
-					// NB. We cannot require blanket directly as it
-					// detects that we are not running mocha cli and loads differently.
-					//
-					// NNB. As mocha is 'clever' enough to only run the tests once for
-					// each file the following coverage task does not actually run any
-					// tests which is why the coverage instrumentation has to be done here
-					require: 'build/config/blanket'
-				},
-				src: ['test/**/*.js']
-			},
-			coverage: {
-				options: {
-					reporter: 'html-cov',
-					// use the quiet flag to suppress the mocha console output
-					quiet: true,
-					// specify a destination file to capture the mocha
-					// output (the quiet option does not suppress this)
-					captureFile: 'build/output/coverage.html'
-				},
-				src: ['test/**/*.js']
-			},
-			// The travis-cov reporter will fail the tests if the
-			// coverage falls below the threshold configured in package.json
-			'travis-cov': {
-				options: {
-					reporter: 'travis-cov'
-				},
-				src: ['test/**/*.js']
-			}
+		meta: {
+			output : 'build/output',
+			src	: 'src/**/*.js',
+			tests : 'test/**/*.js',
+			gruntFile : 'Gruntfile.js',
 		}
 	});
 
+	grunt.registerTask('doc', 
+		function () {
+			
+			grunt.config.requires('meta.output');
+			grunt.config.requires('meta.src');
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-jsdoc');
-	// Add the grunt-mocha-test tasks.
-	grunt.loadNpmTasks('grunt-mocha-test');
+			var output = grunt.config.get('meta.output'),
+				src = grunt.config.get('meta.src'),
+				jsdocConfig =	{
+									src: [src], 
+									dest: output + '/doc'
+								};
 
-	// Create the output folder
-	if(grunt.file.exists('build/output')) {
-		grunt.file.delete('build/output');
-	}
-	grunt.file.mkdir('build/output');
+			if(grunt.file.exists(output)) {
+				grunt.file.delete(output);
+			}
+			grunt.file.mkdir(output);
 
-	grunt.registerTask('test', ['mochaTest']);
+			grunt.config.set('jsdoc.dist', jsdocConfig);
 
-	grunt.registerTask('doc', ['jsdoc']);
+			grunt.loadNpmTasks('grunt-jsdoc');
 
-	// Default task
-	grunt.registerTask('default', ['jshint:all','mochaTest']);
+			grunt.task.run('jsdoc');
+		}
+	);
+
+	grunt.registerTask('lint', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				jshintConfig = {
+					all: [gruntFile, src, tests],
+					code: [src],
+					tests: [tests],
+					options: {
+						ignores: ['node_modules/**/*.js'],
+						globals: {
+							module: true
+						}
+					}
+				};
+
+			switch (target)
+			{
+				case 'code':
+					grunt.config.requires('meta.src');
+					break;
+				case 'tests':
+					grunt.config.requires('meta.tests');
+					break;
+				case 'all':
+					grunt.config.requires('meta.src');
+					grunt.config.requires('meta.tests');
+					break;
+				default:
+					target = 'all';
+			}
+
+			grunt.config.set('jshint', jshintConfig);
+
+			grunt.loadNpmTasks('grunt-contrib-jshint');
+
+			grunt.task.run('jshint:' + target);
+		}
+	);
+
+	grunt.registerTask('tests', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				output = grunt.config.get('meta.output'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				mochaConfig = {
+					test: {
+						options: {
+							reporter: 'spec',
+							// Require blanket wrapper here to instrument other required
+							// files on the fly. 
+							//
+							// NB. We cannot require blanket directly as it
+							// detects that we are not running mocha cli and loads differently.
+							//
+							// NNB. As mocha is 'clever' enough to only run the tests once for
+							// each file the following coverage task does not actually run any
+							// tests which is why the coverage instrumentation has to be done here
+							require: 'build/config/blanket'
+						},
+						src: [tests]
+					},
+					coverage: {
+						options: {
+							reporter: 'html-cov',
+							// use the quiet flag to suppress the mocha console output
+							quiet: true,
+							// specify a destination file to capture the mocha
+							// output (the quiet option does not suppress this)
+							captureFile: output + '/coverage.html'
+						},
+						src: [tests]
+					},
+					// The travis-cov reporter will fail the tests if the
+					// coverage falls below the threshold configured in package.json
+					'travis-cov': {
+						options: {
+							reporter: 'travis-cov'
+						},
+						src: [tests]
+					}
+				};
+
+			if(grunt.file.exists(output)) {
+				grunt.file.delete(output);
+			}
+			grunt.file.mkdir(output);
+
+			grunt.loadNpmTasks('grunt-mocha-test');
+			grunt.config.set('mochaTest', mochaConfig);
+
+			grunt.task.run('mochaTest');
+		}
+	);
+
+	grunt.registerTask('watch', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				watchConfig = {
+					code: {
+						files: src,
+						tasks: ['lint:code', 'tests']
+					},
+					test: {
+						files: tests,
+						tasks: ['lint:tests', 'tests']
+					},
+					all: {
+						files: [src, tests],
+						tasks: ['lint', 'tests']
+					}
+				};
+
+			switch (target)
+			{
+				case 'code':
+					grunt.config.requires('meta.src');
+					break;
+				case 'tests':
+					grunt.config.requires('meta.tests');
+					target = 'test';
+					break;
+				case 'all':
+					grunt.config.requires('meta.src');
+					grunt.config.requires('meta.tests');
+					break;
+				default:
+					target = 'all';
+			}
+
+			grunt.config.set('watch', watchConfig);
+
+			grunt.loadNpmTasks('grunt-contrib-watch');
+
+			grunt.task.run('watch:' + target);
+		}
+	);
+
+	grunt.registerTask('dev', ['lint:all', 'tests']);
+
+	grunt.registerTask('default', ['lint:all', 'tests', 'doc']);
 };
--- templates/code/node-project/Gruntfile.js
+++ templates/code/node-project/Gruntfile.js
@@ -9,106 +9,186 @@ module.exports = function (grunt) {
 
 		//Read the package.json (optional)
 		pkg: grunt.file.readJSON('package.json'),
-		
-		meta : {
-			paths : {
-				all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-				code: ['src/**/*.js'],
-				tests: ['test/**/*.js']
-			}
-		},
-
-		jsdoc : {
-			dist : {
-				src: '<%= meta.paths.code %>', 
-				dest: 'build/output/doc'
-			}
-		},
-		jshint: {
-			all: '<%= meta.paths.all %>',
-			code: '<%= meta.paths.code %>',
-			tests: '<%= meta.paths.tests %>',
-			options: {
-				ignores: ['node_modules/**/*.js'],
-				globals: {
-					module: true
-				}
-			}
-		},
-		watch: {
-			all: {
-				files: '<%= meta.paths.all %>',
-				tasks: ['jshint:all', 'mochaTest']
-			},
-			code: {
-				files: '<%= meta.paths.code %>',
-				tasks: ['jshint:code', 'mochaTest']
-			},
-			test: {
-				files: '<%= meta.paths.tests %>',
-				tasks: ['jshint:tests', 'mochaTest']
-			}
-		},
-		// Configure a mochaTest task
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec',
-					// Require blanket wrapper here to instrument other required
-					// files on the fly. 
-					//
-					// NB. We cannot require blanket directly as it
-					// detects that we are not running mocha cli and loads differently.
-					//
-					// NNB. As mocha is 'clever' enough to only run the tests once for
-					// each file the following coverage task does not actually run any
-					// tests which is why the coverage instrumentation has to be done here
-					require: 'build/config/blanket'
-				},
-				src: '<%= meta.paths.tests %>'
-			},
-			coverage: {
-				options: {
-					reporter: 'html-cov',
-					// use the quiet flag to suppress the mocha console output
-					quiet: true,
-					// specify a destination file to capture the mocha
-					// output (the quiet option does not suppress this)
-					captureFile: 'build/output/coverage.html'
-				},
-				src: '<%= meta.paths.tests %>'
-			},
-			// The travis-cov reporter will fail the tests if the
-			// coverage falls below the threshold configured in package.json
-			'travis-cov': {
-				options: {
-					reporter: 'travis-cov'
-				},
-				src: '<%= meta.paths.tests %>'
-			}
+		meta: {
+			output : 'build/output',
+			src	: 'src/**/*.js',
+			tests : 'test/**/*.js',
+			gruntFile : 'Gruntfile.js',
 		}
 	});
 
+	grunt.registerTask('doc', 
+		function () {
+			
+			grunt.config.requires('meta.output');
+			grunt.config.requires('meta.src');
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-jsdoc');
-	// Add the grunt-mocha-test tasks.
-	grunt.loadNpmTasks('grunt-mocha-test');
+			var output = grunt.config.get('meta.output'),
+				src = grunt.config.get('meta.src'),
+				jsdocConfig =	{
+									src: [src], 
+									dest: output + '/doc'
+								};
 
-	// Create the output folder
-	if(grunt.file.exists('build/output')) {
-		grunt.file.delete('build/output');
-	}
-	grunt.file.mkdir('build/output');
+			if(grunt.file.exists(output)) {
+				grunt.file.delete(output);
+			}
+			grunt.file.mkdir(output);
 
-	grunt.registerTask('test', ['mochaTest']);
+			grunt.config.set('jsdoc.dist', jsdocConfig);
 
-	grunt.registerTask('doc', ['jsdoc']);
+			grunt.loadNpmTasks('grunt-jsdoc');
 
-	grunt.registerTask('build', ['jshint:all','mochaTest','jsdoc']);
+			grunt.task.run('jsdoc');
+		}
+	);
 
-	// Default task
-	grunt.registerTask('default', ['jshint:all','mochaTest']);
+	grunt.registerTask('lint', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				jshintConfig = {
+					all: [gruntFile, src, tests],
+					code: [src],
+					tests: [tests],
+					options: {
+						ignores: ['node_modules/**/*.js'],
+						globals: {
+							module: true
+						}
+					}
+				};
+
+			switch (target)
+			{
+				case 'code':
+					grunt.config.requires('meta.src');
+					break;
+				case 'tests':
+					grunt.config.requires('meta.tests');
+					break;
+				case 'all':
+					grunt.config.requires('meta.src');
+					grunt.config.requires('meta.tests');
+					break;
+				default:
+					target = 'all';
+			}
+
+			grunt.config.set('jshint', jshintConfig);
+
+			grunt.loadNpmTasks('grunt-contrib-jshint');
+
+			grunt.task.run('jshint:' + target);
+		}
+	);
+
+	grunt.registerTask('tests', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				output = grunt.config.get('meta.output'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				mochaConfig = {
+					test: {
+						options: {
+							reporter: 'spec',
+							// Require blanket wrapper here to instrument other required
+							// files on the fly. 
+							//
+							// NB. We cannot require blanket directly as it
+							// detects that we are not running mocha cli and loads differently.
+							//
+							// NNB. As mocha is 'clever' enough to only run the tests once for
+							// each file the following coverage task does not actually run any
+							// tests which is why the coverage instrumentation has to be done here
+							require: 'build/config/blanket'
+						},
+						src: [tests]
+					},
+					coverage: {
+						options: {
+							reporter: 'html-cov',
+							// use the quiet flag to suppress the mocha console output
+							quiet: true,
+							// specify a destination file to capture the mocha
+							// output (the quiet option does not suppress this)
+							captureFile: output + '/coverage.html'
+						},
+						src: [tests]
+					},
+					// The travis-cov reporter will fail the tests if the
+					// coverage falls below the threshold configured in package.json
+					'travis-cov': {
+						options: {
+							reporter: 'travis-cov'
+						},
+						src: [tests]
+					}
+				};
+
+			if(grunt.file.exists(output)) {
+				grunt.file.delete(output);
+			}
+			grunt.file.mkdir(output);
+
+			grunt.loadNpmTasks('grunt-mocha-test');
+			grunt.config.set('mochaTest', mochaConfig);
+
+			grunt.task.run('mochaTest');
+		}
+	);
+
+	grunt.registerTask('watch', 
+		function (target) {
+
+			var src = grunt.config.get('meta.src'),
+				tests = grunt.config.get('meta.tests'),
+				gruntFile = grunt.config.get('meta.gruntFile'),
+				watchConfig = {
+					code: {
+						files: src,
+						tasks: ['lint:code', 'tests']
+					},
+					test: {
+						files: tests,
+						tasks: ['lint:tests', 'tests']
+					},
+					all: {
+						files: [src, tests],
+						tasks: ['lint', 'tests']
+					}
+				};
+
+			switch (target)
+			{
+				case 'code':
+					grunt.config.requires('meta.src');
+					break;
+				case 'tests':
+					grunt.config.requires('meta.tests');
+					target = 'test';
+					break;
+				case 'all':
+					grunt.config.requires('meta.src');
+					grunt.config.requires('meta.tests');
+					break;
+				default:
+					target = 'all';
+			}
+
+			grunt.config.set('watch', watchConfig);
+
+			grunt.loadNpmTasks('grunt-contrib-watch');
+
+			grunt.task.run('watch:' + target);
+		}
+	);
+
+	grunt.registerTask('dev', ['lint:all', 'tests']);
+
+	grunt.registerTask('default', ['lint:all', 'tests', 'doc']);
 };
