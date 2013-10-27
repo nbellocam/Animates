ee7b6bbf498045b71fe194f52e3ab39e61b16e5c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-10-27 20:43:51 -0300

    Renamed shapeFrame to mediaFrame

---
+++ code/client-model/src/mediaFrame.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ *  Creates a new MediaFrame.
+ *  @class Represents the specific frame of a media object. 
+ */
+function MediaFrame (options) {
+	var _self = this; // Save the this reference for later use
+
+	/**
+	 *  Constructor
+	 */
+	(function init() {
+	}());
+}
+
+module.exports = MediaFrame;
