4cb7c2961dafe206f1a48cd657488f2bfaa29604
Pablo Biasotti <pbiasotti@hotmail.com>
2013-10-27 19:43:42 -0300

    Added new MediaObjectClass

---
+++ code/client-model/src/mediaObject.js
@@ -0,0 +1,34 @@
+/*global Animates */
+/*jslint node: true, todo: true, white: true, plusplus:true */
+
+var Common = require('animates-common');
+
+'use strict';
+
+/**
+ *  Creates a new MediaObject
+ *  @class Represents a MediaObject. 
+ */
+function MediaObject (options) {
+	options = options || {};
+
+	var _self = this,
+		guid = '';
+
+	/**
+	 * Gets the guid of the MediaObject
+	 * @return {string} the guid
+	 */
+	this.getGuid = function getGuid() {
+		return guid;
+	};
+
+	/**
+	 *  Constructor
+	 */ 
+	(function init() {
+		guid = Common.createGuid();
+	}());
+}
+
+module.exports = MediaObject;
---
+++ code/client-model/test/unit/mediaObject.js
@@ -0,0 +1,16 @@
+/*global require, describe, it */
+
+var MediaObject = require('../../src/mediaObject'),
+	should = require("should");
+
+describe('MediaObject', function(){
+	describe('Constructor', function(){
+		it('Should generate a random guid', function(){
+			var mediaObject = new MediaObject(),
+				mediaObject2 = new MediaObject();
+
+			mediaObject.getGuid().should.have.type('string');
+			mediaObject.getGuid().should.not.be.equal(mediaObject2.getGuid());
+		});
+	});
+});
