935c0620b39ba4ec7270b9e3c6426e2effdc90e7
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-11-01 17:54:38 -0300

    Added model class as main class to export

---
+++ code/client-model/src/model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var model = {
+	Effect : require('./effect'),
+	MediaFrame : require('./mediaFrame'),
+	MediaObject : require('./mediaObject'),
+	Shape : require('./shape'),
+	VisualMediaObject : require('./visualMediaObject')
+};
+
+ module.exports = model;
