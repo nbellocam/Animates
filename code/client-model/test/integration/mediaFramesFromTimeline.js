e1630b8084f1ed18d25a81effd57c637a0f1521b
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-11-15 16:02:06 -0300

    Added test for two rectangles with effects

--- code/client-model/test/integration/mediaFramesFromTimeline.js
+++ code/client-model/test/integration/mediaFramesFromTimeline.js
@@ -232,5 +232,151 @@ describe('Retrive all MediaFrames for a specific frame number from the Timeline'
 			properties.should.have.property('height', specifiedProperties.height);
 			properties.should.have.property('width', specifiedProperties.width);
 		});
+
+		it('Should retrive the MediaFrame objects related to the two rectangles, one of them updated with the move effect', function(){
+			var specifiedProperties = {
+					position : {
+						x : 0,
+						y : 0,
+						z : 0
+					},
+					opacity: 42,
+					border : {
+						type : 'border',
+						color : 'blue'
+					},
+					height: 32,
+					width: 84
+				},
+				rectangle1 = new Rectangle(specifiedProperties),
+				rectangle2 = new Rectangle(specifiedProperties),
+				timeline = new Timeline(),
+				specificCurrentFrameNumber = 42,
+				specificCurrentFrameNumber2 = specificCurrentFrameNumber / 2,
+				specifiedEndPosition = { x: specificCurrentFrameNumber, y: specificCurrentFrameNumber },
+				specifiedPath = new Path({ endPosition : specifiedEndPosition }),
+				moveEffect = new MoveEffect({ path: specifiedPath }),
+				mediaTimeline,
+				mediaFramesCollection,
+				mediaFrame,
+				properties;
+
+			timeline.addMediaObject(rectangle1);
+			timeline.addMediaObject(rectangle2);
+
+			//First rectangle add effect
+			mediaTimeline = timeline.getMediaTimeline(rectangle1.getGuid());
+			should.exists(mediaTimeline);
+			mediaTimeline.should.be.instanceOf(MediaTimeline);
+			
+			mediaTimeline.addEffect(moveEffect);
+			mediaTimeline.getEffects().should.have.property(moveEffect.getGuid());
+
+			mediaTimeline = timeline.getMediaTimeline(rectangle1.getGuid());
+			mediaTimeline.getEffects().should.have.property(moveEffect.getGuid());
+
+			//Second rectangle add effect
+			mediaTimeline = timeline.getMediaTimeline(rectangle1.getGuid());
+			should.exists(mediaTimeline);
+			mediaTimeline.should.be.instanceOf(MediaTimeline);
+			
+			mediaTimeline.addEffect(moveEffect);
+			mediaTimeline.getEffects().should.have.property(moveEffect.getGuid());
+
+			mediaTimeline = timeline.getMediaTimeline(rectangle1.getGuid());
+			mediaTimeline.getEffects().should.have.property(moveEffect.getGuid());
+
+
+			//First time that getElementsForFrame
+			mediaFramesCollection = timeline.getElementsForFrame(specificCurrentFrameNumber);
+			should.exists(mediaFramesCollection);
+			mediaFramesCollection.should.have.lengthOf(2);
+
+			// First rectangle
+			mediaFrame = mediaFramesCollection[0];
+
+			should.exists(mediaFrame);
+			mediaFrame.should.be.instanceOf(MediaFrame);
+
+			properties = mediaFrame.properties();
+
+			properties.should.have.property('position');
+			properties.position.should.have.property('x', specificCurrentFrameNumber);
+			properties.position.should.have.property('y', specificCurrentFrameNumber);
+			properties.position.should.have.property('z', specifiedProperties.position.z);
+			properties.should.have.property('opacity', specifiedProperties.opacity);
+			properties.should.have.property('border');
+			properties.border.should.have.property('type', specifiedProperties.border.type);
+			properties.border.should.have.property('color', specifiedProperties.border.color);
+
+			properties.should.have.property('height', specifiedProperties.height);
+			properties.should.have.property('width', specifiedProperties.width);
+
+			// Second rectangle
+			mediaFrame = mediaFramesCollection[0];
+
+			should.exists(mediaFrame);
+			mediaFrame.should.be.instanceOf(MediaFrame);
+
+			properties = mediaFrame.properties();
+
+			properties.should.have.property('position');
+			properties.position.should.have.property('x', specificCurrentFrameNumber);
+			properties.position.should.have.property('y', specificCurrentFrameNumber);
+			properties.position.should.have.property('z', specifiedProperties.position.z);
+			properties.should.have.property('opacity', specifiedProperties.opacity);
+			properties.should.have.property('border');
+			properties.border.should.have.property('type', specifiedProperties.border.type);
+			properties.border.should.have.property('color', specifiedProperties.border.color);
+
+			properties.should.have.property('height', specifiedProperties.height);
+			properties.should.have.property('width', specifiedProperties.width);
+
+
+			//Second time that getElementsForFrame
+			mediaFramesCollection = timeline.getElementsForFrame(specificCurrentFrameNumber2);
+			should.exists(mediaFramesCollection);
+			mediaFramesCollection.should.have.lengthOf(2);
+
+			// First rectangle
+			mediaFrame = mediaFramesCollection[0];
+
+			should.exists(mediaFrame);
+			mediaFrame.should.be.instanceOf(MediaFrame);
+
+			properties = mediaFrame.properties();
+
+			properties.should.have.property('position');
+			properties.position.should.have.property('x', specificCurrentFrameNumber2);
+			properties.position.should.have.property('y', specificCurrentFrameNumber2);
+			properties.position.should.have.property('z', specifiedProperties.position.z);
+			properties.should.have.property('opacity', specifiedProperties.opacity);
+			properties.should.have.property('border');
+			properties.border.should.have.property('type', specifiedProperties.border.type);
+			properties.border.should.have.property('color', specifiedProperties.border.color);
+
+			properties.should.have.property('height', specifiedProperties.height);
+			properties.should.have.property('width', specifiedProperties.width);
+
+			// Second rectangle
+			mediaFrame = mediaFramesCollection[1];
+
+			should.exists(mediaFrame);
+			mediaFrame.should.be.instanceOf(MediaFrame);
+
+			properties = mediaFrame.properties();
+
+			properties.should.have.property('position');
+			properties.position.should.have.property('x', specificCurrentFrameNumber2);
+			properties.position.should.have.property('y', specificCurrentFrameNumber2);
+			properties.position.should.have.property('z', specifiedProperties.position.z);
+			properties.should.have.property('opacity', specifiedProperties.opacity);
+			properties.should.have.property('border');
+			properties.border.should.have.property('type', specifiedProperties.border.type);
+			properties.border.should.have.property('color', specifiedProperties.border.color);
+
+			properties.should.have.property('height', specifiedProperties.height);
+			properties.should.have.property('width', specifiedProperties.width);
+		});
 	});
 });
