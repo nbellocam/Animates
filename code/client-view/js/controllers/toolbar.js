a49998d1a366f4d08980b9fb2fe351dcc9d9c276
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2013-12-29 16:52:51 -0300

    Implemented remove item (using selection)

--- code/client-view/js/controllers/toolbar.js
+++ code/client-view/js/controllers/toolbar.js
@@ -5,29 +5,54 @@ animates.controller('ToolbarCtrl', function($scope, shapeCreator, currentCanvas)
       if (currentCanvas.instance){
         currentCanvas.instance.add(shapeCreator.createRectangle());
       }
-    }
+    };
 
     $scope.addCircle = function() {
       alert("Circle still not available!");
-    }
+    };
 
     $scope.addTriangle = function() {
       alert("Triangle still not available!");
-    }
+    };
 
     $scope.addStar = function() {
       alert("Star still not available!");
-    }
+    };
 
     $scope.addImage = function() {
       alert("Image still not available!");
-    }
+    };
 
     $scope.addText = function() {
       alert("Text still not available!");
-    }
+    };
 
     $scope.addSound = function() {
       alert("Sound still not available!");
-    }
+    };
+
+    $scope.removeElements = function() {
+      var canvas = currentCanvas.instance;
+
+      if (canvas){
+        var allObjects = canvas.getObjects(),
+          j, object, objects;
+
+        for (var i = 0; i < allObjects.length; i++) {
+          object = allObjects[i]
+          if (object.active) {
+            if (object.group) {
+              objects = object.group.objects;
+              for (j = objects.length - 1; j >= 0; j--) {
+                canvas.remove(objects[j]);
+              };
+            } else {
+              canvas.remove(object);
+            }
+          }
+        }
+      }
+      
+      canvas.renderAll();
+    };
 });
