334dcfb40fc546fadc15ca2c419b45a3c6887291
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-01-05 13:20:15 -0300

    Added canvas to model

--- code/model/src/canvas.js
+++ code/model/src/canvas.js
@@ -7,7 +7,10 @@
 function Canvas (options) {
 	var _self = this; // Save the this reference for later use
 
-	// TODO
+	this.height = options.height || 100;
+	this.width = options.width || 100;
+	this.backgroundColor = options.backgroundColor || 'white';
+	this.backgroundImage = options.backgroundImage || '';
 
 	/**
 	 *  Constructor
