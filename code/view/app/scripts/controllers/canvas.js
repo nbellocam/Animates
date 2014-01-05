35741207f4dd8033eca6411d2fc504eb4292c766
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-01-05 13:45:43 -0300

    Updated canvas controller to include canvas model

--- code/view/app/scripts/controllers/canvas.js
+++ code/view/app/scripts/controllers/canvas.js
@@ -2,20 +2,33 @@
 
 angular.module('animatesApp')
 	.controller('CanvasCtrl', function($scope, shapeCreator, currentCanvas, shapeSync) {
-		var canvas = new fabric.Canvas('mainCanvas');
+		var createCanvas = function createCanvas(id) {
+				var canvas = new fabric.Canvas(id);
+				canvas.model = new model.Canvas();
+				// Update Properties
+
+				canvas.on('object:modified', function(options) {
+					if (options.target) {
+						//console.log('an object was clicked! ', options.target.type);
+						shapeSync.syncFromFabric(options.target);
+						//TODO work with the diff that is returned by the shapeSync.syncFromFabric method.
+					}
+				});
+
+				return canvas;
+			},
+			canvas = createCanvas('mainCanvas');;
+		
 		currentCanvas.instance = canvas;
 
 		$scope.initialize = function() {
-			canvas.setHeight(500);
-			canvas.setWidth(800);
 
-			canvas.on('object:modified', function(options) {
-				if (options.target) {
-					//console.log('an object was clicked! ', options.target.type);
-					shapeSync.syncFromFabric(options.target);
-					//TODO work with the diff that is returned by the shapeSync.syncFromFabric method.
-				}
-			});
+			//TODO create helper functions to set the canvas properties.
+			canvas.setHeight(500);
+			canvas.model.height = 500;
+
+			canvas.setWidth(800);
+			canvas.model.width = 800;
 
 			var rect = shapeCreator.createRectangle();
 			canvas.add(rect);
