43a5d43bf080aadf58b94e5f365d3a5f11bd4ed6
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-01-05 12:51:05 -0300

    minor update

--- code/view/app/scripts/services/canvasutils.js
+++ code/view/app/scripts/services/canvasutils.js
@@ -1,31 +1,31 @@
 'use strict';
 
 angular.module('animatesApp')
-  .factory('canvasUtils', function (currentCanvas) {
-    var applyToActiveElements = function applyToActiveElements(activeElementFunction){
-      var canvas = currentCanvas.instance;
+	.factory('canvasUtils', function (currentCanvas) {
+		var applyToActiveElements = function applyToActiveElements(activeElementFunction){
+			var canvas = currentCanvas.instance;
 
-      if (canvas){
-        var allObjects = canvas.getObjects(),
-          j, object, objects;
+			if (canvas){
+				var allObjects = canvas.getObjects(),
+					j, object, objects;
 
-        for (var i = 0; i < allObjects.length; i++) {
-          object = allObjects[i]
-          if (object.active) {
-            if (object.group) {
-              objects = object.group.objects;
-              for (j = objects.length - 1; j >= 0; j--) {
-                activeElementFunction(objects[j]);
-              };
-            } else {
-              activeElementFunction(object);
-            }
-          }
-        }
-      }
-    };
-    
-    return {
-      applyToActiveElements : applyToActiveElements
-    };
-  });
+				for (var i = 0; i < allObjects.length; i++) {
+					object = allObjects[i]
+					if (object.active) {
+						if (object.group) {
+							objects = object.group.objects;
+							for (j = objects.length - 1; j >= 0; j--) {
+								activeElementFunction(objects[j]);
+							};
+						} else {
+							activeElementFunction(object);
+						}
+					}
+				}
+			}
+		};
+		
+		return {
+			applyToActiveElements : applyToActiveElements
+		};
+	});
--- code/view/app/scripts/services/shapecreator.js
+++ code/view/app/scripts/services/shapecreator.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('animatesApp')
-  .factory('shapeCreator', function (shapeSync) {
-    var createRectangle = function createRectangle(){
-      // TODO: we need to pass the model and fabric in the correct ways to angular.
-      var rect = new fabric.Rect({
-          left: 50, top: 50, fill: 'red', width: 70, height: 150
-        });
+	.factory('shapeCreator', function (shapeSync) {
+		var createRectangle = function createRectangle(){
+			// TODO: we need to pass the model and fabric in the correct ways to angular.
+			var rect = new fabric.Rect({
+					left: 50, top: 50, fill: 'red', width: 70, height: 150
+				});
 
-      rect.model = new model.Rectangle();
-      shapeSync.syncFromFabric(rect);
-      return rect;
-    };
-    
-    return {
-      createRectangle : createRectangle
-    };
-  });
+			rect.model = new model.Rectangle();
+			shapeSync.syncFromFabric(rect);
+			return rect;
+		};
+		
+		return {
+			createRectangle : createRectangle
+		};
+	});
