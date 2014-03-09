fb88e43b43f638ba201cdb0732fb15abe93e10b2
KidD32 <pbiasotti@hotmail.com>
2014-03-09 12:46:16 -0300

    TimelinePanel hardcoded first layout

---
+++ code/view/app/scripts/controllers/timelinePanel.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('animatesApp')
+	.controller('TimelinePanelCtrl', function($scope, $rootScope, $location, canvasService, canvasUtils) {
+		$scope.properties = null;
+
+		$scope.empty = function () {
+			var isEmpty = ($scope.properties === null);
+			return isEmpty;
+		};
+
+		$rootScope.$on('selectedShapeChange', function (event, canvasShape){
+			if (canvasShape === null) {
+				$scope.properties = null;
+			} else {
+				$scope.properties = canvasShape.model.getProperties();
+			}
+			$scope.$apply();
+		});
+
+		$rootScope.$on('shapeChange', function (event, canvasShape){
+			if (canvasService.getSelectedShape().model.getGuid() === canvasShape.model.getGuid()) {
+				$scope.properties = canvasService.getSelectedShape().model.getProperties();
+				$scope.$apply();
+			}
+		});
+	});
