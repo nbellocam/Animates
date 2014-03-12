ad5cc22b5d64ca17a541747884c591e4181dc444
KidD32 <pbiasotti@hotmail.com>
2014-03-12 00:13:55 -0300

    Timeline draw model timelines
    The now draw real model timelines. Remove of timelines is not working
    correctly

--- code/view/app/scripts/controllers/timelinePanel.js
+++ code/view/app/scripts/controllers/timelinePanel.js
@@ -2,41 +2,38 @@
 
 angular.module('animatesApp')
 	.controller('TimelinePanelCtrl', function($scope, $rootScope, $location, canvasService, timelineService) {
-		$scope.timelines = [{
-			guid : 'timeline1',
-			name : 'timeline1-name',
-			events : [{
-					name : 'animation1',
-					start : 10,
-					duration : 190,
-					'class' : 'fade-out',
-				},
-				{
-					name : 'animation2',
-					start : 210,
-					duration : 200,
-					'class' : 'fade-in',
-				}]
-			}];
+		$scope.timelineService = timelineService;
 
-		$scope.empty = function () {
-			var isEmpty = ($scope.properties === null);
-			return isEmpty;
+		$scope.timelines = [];
+		
+		$scope.$watchCollection('timelineService.getMediaTimelines()', function (newVal, oldVal) {
+			console.log('coll changed');
+			$scope.adaptMediaTimelines(newVal);
+		});
+
+		$scope.adaptMediaTimelines = function adaptMediaTimelines (mediaTimelines){
+			$scope.timelines = [];
+			angular.forEach(mediaTimelines, function (mediaTimeline){
+				var timeline = { 
+					guid : mediaTimeline.getMediaObjectId(),
+					name : mediaTimeline.getMediaObjectId(),
+					events : []
+				};
+
+				angular.forEach(mediaTimeline.getEffects(), function (effect){
+					timeline.events.push(
+						{
+							name : effect.getGuid(),
+							start : effect.getStartTick(),
+							duration : effect.getEndTick() - effect.getStartTick()
+						}
+					);
+				});
+				$scope.timelines.push(timeline);
+			});
 		};
 
-		$rootScope.$on('selectedShapeChange', function (event, canvasShape){
-			if (canvasShape === null) {
-				$scope.properties = null;
-			} else {
-				$scope.properties = canvasShape.model.properties;
-			}
-			$scope.$apply();
-		});
-
-		$rootScope.$on('shapeChange', function (event, canvasShape){
-			if (canvasService.getSelectedShape().model.getMediaObjectGuid() === canvasShape.model.getMediaObjectGuid()) {
-				$scope.properties = canvasService.getSelectedShape().model.properties();
-				$scope.$apply();
-			}
-		});
+		$scope.empty = function () {
+			return ($scope.timelines.length === 0);
+		};
 	});
