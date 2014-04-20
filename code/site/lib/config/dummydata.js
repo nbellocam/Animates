096540b4dfd4858a88e3472b4a5e89fdfd09b05d
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-04-19 22:51:36 -0300

    test build flow

--- code/site/lib/config/dummydata.js
+++ code/site/lib/config/dummydata.js
@@ -13,19 +13,23 @@ Thing.find({}).remove(function() {
   Thing.create({
     name : 'HTML5 Boilerplate',
     info : 'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.',
-    awesomeness: 10
+    awesomeness: 8
   }, {
     name : 'AngularJS',
     info : 'AngularJS is a toolset for building the framework most suited to your application development.',
-    awesomeness: 20
+    awesomeness: 10
   }, {
     name : 'Express',
     info : 'Flexible and minimalist web application framework for node.js.',
     awesomeness: 10
+  },  {
+    name : 'Grunt.js',
+    info : 'Build the project',
+    awesomeness: 9
   }, {
     name : 'MongoDB + Mongoose',
     info : 'An excellent document database. Combined with Mongoose to simplify adding validation and business logic.',
-    awesomeness: 10
+    awesomeness: 7
   }, function() {
       console.log('finished populating things');
     }
