89e43eecf4a2f5e903ba81acf3fcb8f8165dc25b
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-05-03 00:34:41 -0300

    Added buildSite script

---
+++ buildSite.sh
@@ -0,0 +1,19 @@
+if [ -d "src" ]; then
+    rm -rf src
+fi
+
+mv build/site/site src
+
+mkdir -p src/views/partials/editor
+mv build/view/site/views/* src/views/partials/editor/
+mv build/view/site/styles/editor-* src/public/styles/
+mv build/view/site/scripts/editor-* src/public/scripts/
+mv build/view/site/images/* src/public/images/
+
+rm -rf build/
+
+git add . --all
+
+if [ -n "$(git status --porcelain)" ]; then 
+     git commit -m "$BUILD_TAG"
+fi
