a5bc005c1c7e434a41dd87887e606bbfa66da86e
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-05-07 22:52:36 -0300

    Removed old server projects as they are not longer useful

--- code/old-site/public/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.projects']);
-
-angular.module('mean.system', []);
-angular.module('mean.articles', []);
-angular.module('mean.projects', []);
