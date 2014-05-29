0b5e0213d91b27406cb5510c278cf60cf5681d89
KidD32 <pbiasotti@hotmail.com>
2014-05-28 23:57:48 -0300

    Shape properties refactor

--- code/model/src/shape.js
+++ code/model/src/shape.js
@@ -2,17 +2,18 @@
 
 var VisualMediaObject = require('./visualMediaObject'),
 	JsonSerializer = require('./serialization/jsonSerializer'),
+	PropertiesArrayBuilder = require('./properties/propertiesArrayBuilder'),
 	Common = require('animates-common');
 
 /**
  *  Creates a new Shape
  *  @class Represents a Shape. 
  */
-function Shape (options) {
+function Shape (options, builder) {
 	var _self = this,
-		properties = options || {};
-	
-	this.VisualMediaObject(properties); // Call base constructor
+		propBuilder,
+		properties,
+		defaultOptions = {};
 
 	this.mediaObject_toJSON = this.toJSON;
 	this.toJSON = function () {
@@ -28,6 +29,10 @@ function Shape (options) {
 	 *  Constructor
 	 */ 
 	(function init() {
+		propBuilder = builder || new PropertiesArrayBuilder();
+		options = Common.extend(options || {}, defaultOptions);
+
+		_self.VisualMediaObject(options, propBuilder); // Call base constructor
 	}());
 }
 
