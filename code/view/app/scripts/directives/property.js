eca506a9e173c8d2534fc3d61a7fd0a7ad26607f
KidD32 <pbiasotti@hotmail.com>
2014-05-30 16:04:07 -0300

    Fix: properties where not being correctly shown in the panel
    Properties where not being correctly shown when the selected shape was
    modified in the canvas (or remotely)
    Some lint fixes where done.

--- code/view/app/scripts/directives/property.js
+++ code/view/app/scripts/directives/property.js
@@ -4,20 +4,21 @@ angular.module('animatesApp')
 	.directive('animatesProperty', function () {
 		return {
 			restrict: 'E',
-			scope: { 
+			scope: {
 				propkey: '=',
 				prop: '='
 			},
-			template : 	"<input type='' value='{{ prop.value() }}' id='property.{{ propkey }}' ng-blur='propertyBlur($event)'></input>",
+			template: '<input type="" value="{{ prop.value() }}" id="property.{{ propkey }}" ng-blur="propertyBlur($event)"></input>',
 			controller: function($scope) {
 				$scope.isValid = true;
 				$scope.propertyBlur = function (event) {
 					var oldValue = $scope.prop.value(),
 						newValue = event.target.value;
 					
+					//TODO controller should not be accesing the dom
 					event.target.style.color = 'black';
 
-					if (oldValue != newValue) {
+					if (oldValue !== newValue) {
 						if ($scope.prop.isValid(newValue)) {
 						} else {
 							event.target.style.color = 'red';
