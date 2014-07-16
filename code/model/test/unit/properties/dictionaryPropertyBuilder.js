8f3663a405e9d3c45692b8955f512568b3c53130
KidD32 <pbiasotti@hotmail.com>
2014-07-15 23:11:34 -0300

    Complex points tests added to DictionaryPropertyBuilder

--- code/model/test/unit/properties/dictionaryPropertyBuilder.js
+++ code/model/test/unit/properties/dictionaryPropertyBuilder.js
@@ -12,14 +12,14 @@ describe('DictionaryPropertyBuilder', function() {
 	var complexValues = {
 			'id1' : {
 				'tick' : 100,
-				'postion' : {
+				'position' : {
 					'x' : 10,
 					'y' : 20
 				}
 			},
 			'id2' : {
 				'tick' : 500,
-				'postion' : {
+				'position' : {
 					'x' : 44,
 					'y' : 66
 				}
@@ -47,7 +47,47 @@ describe('DictionaryPropertyBuilder', function() {
 							.add()
 							.values(simpleValues)
 							.create();
+
 		properties.names().should.have.lengthOf(2);
 		properties.names().should.have.eql(['id1.name', 'id2.name']);
+	});
+
+	it('Should create a complex dictionary', function () {
+		var propBuilder = new DictionaryPropertyBuilder();
+		var properties = propBuilder
+							.name('Points')
+							.schema(CompositePropertyBuilder)
+								.property('tick', PropertyBuilder)
+									.type('float')
+								.add()
+								.property('position', CompositePropertyBuilder)
+									.property('x', PropertyBuilder)
+										.value(0)
+										.type('float')
+									.add()
+									.property('y', PropertyBuilder)
+										.value(0)
+										.type('float')
+									.add()
+								.add()
+							.add()
+							.values(complexValues)
+							.create();
+
+		properties.names().should.have.lengthOf(6);
+		properties.names().should.have.eql(['id1.tick', 'id1.position.x', 'id1.position.y',
+											'id2.tick', 'id2.position.x', 'id2.position.y'
+											]);
+
+		properties.getValue('id1.tick').should.equal(100);
+		properties.getValue('id1.position.x').should.equal(10);
+		properties.getValue('id1.position.y').should.equal(20);
+
+		properties.getValue('id2.tick').should.equal(500);
+		properties.getValue('id2.position.x').should.equal(44);
+		properties.getValue('id2.position.y').should.equal(66);
+
+		// Another way of getting the same values
+		properties.get('id2').getValue('position.x').should.equal(44);
 	});
 });
