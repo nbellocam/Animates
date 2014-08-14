d645ed7e1b68b3b61cc2fe256e71f8a2178c80d8
KidD32 <pbiasotti@hotmail.com>
2014-08-14 19:23:03 -0300

    Circle Added to model

---
+++ code/model/src/shapes/circle.js
@@ -0,0 +1,56 @@
+/*global Animates */
+/*global Animates */
+/*jslint node: true, todo: true, white: true, plusplus:true */
+
+'use strict';
+
+var Shape = require('../shape'),
+	JsonSerializer = require('../serialization/jsonSerializer'),
+	PropertyBuilder = require('../properties/propertyBuilder'),
+	CompositePropertyBuilder = require('../properties/compositePropertyBuilder'),
+	Common = require('animates-common');
+
+/**
+ *  Creates a new Circle
+ *  @class Represents a Circle. 
+ */
+function Circle (options, builder) {
+	var _self = this,
+		propBuilder,
+		properties,
+		defaultOptions = {
+			radius : 100
+		};
+
+	this.shape_toJSON = this.toJSON;
+	this.toJSON = function () {
+		return _self.shape_toJSON();
+	};
+
+	this.shape_fromJSON = this.fromJSON;
+	this.fromJSON = function (json) {
+		_self.shape_fromJSON(json);
+	};
+
+	/**
+	 *  Constructor
+	 */ 
+	(function init() {
+		propBuilder = builder || new CompositePropertyBuilder();
+		options = Common.extend(options || {}, defaultOptions);
+
+		propBuilder.property('radius', PropertyBuilder)
+						.value(options.radius)
+						.type('float')
+						.constraint(function (val) { return (val >= 0); })
+					.add();
+
+		_self.Shape(options, propBuilder); // Call base constructor
+	}());
+}
+
+Common.inherits(Circle, Shape, 'Shape');
+
+JsonSerializer.registerType(Circle);
+
+module.exports = Circle;
---
+++ code/model/test/unit/shapes/circle.js
@@ -0,0 +1,114 @@
+/*global describe, it */
+
+'use strict';
+
+var Circle = require('../../../src/shapes/circle'),
+	should = require("should");
+
+describe('Circle', function() {
+	describe('constructor', function() {
+		it('Should generate a random guid.', function() {
+			var circle = new Circle(),
+				circle2 = new Circle();
+
+			circle.getGuid().should.have.type('string');
+			circle.getGuid().should.not.be.equal(circle2.getGuid());
+		});
+
+		it('Should retrieve Circle as type.', function() {
+			var circle = new Circle();
+
+			circle.getType().should.be.equal('Circle');
+		});
+		
+		it('Should set default properties.', function() {
+			var circle = new Circle();
+
+			circle.getProperty('position.x').should.equal(0);
+			circle.getProperty('position.y').should.equal(0);
+			circle.getProperty('position.z').should.equal(0);
+			circle.getProperty('opacity').should.equal(1);
+			circle.getProperty('border.type').should.equal('solid');
+			circle.getProperty('border.color').should.equal('#000000');
+
+			circle.getProperty('radius').should.equal(100);
+		});
+
+		it('Should set the properties passed in the constructor.', function() {
+			var specifiedX = 1,
+				specifiedY = 2,
+				specifiedZ = 3,
+				specifiedOpacity = 0.8,
+				specifiedBorderType = 'solid',
+				specifiedBorderColor = 'blue',
+				specifiedRadius = 32,
+				circle = new Circle({
+					position : {
+						x : specifiedX,
+						y : specifiedY,
+						z : specifiedZ
+					},
+					opacity: specifiedOpacity,
+					border : {
+						type : specifiedBorderType,
+						color : specifiedBorderColor
+					},
+					radius: specifiedRadius,
+				});
+
+			circle.getProperty('position.x').should.equal(specifiedX);
+			circle.getProperty('position.y').should.equal(specifiedY);
+			circle.getProperty('position.z').should.equal(specifiedZ);
+			circle.getProperty('opacity').should.equal(specifiedOpacity);
+			circle.getProperty('border.type').should.equal(specifiedBorderType);
+			circle.getProperty('border.color').should.equal(specifiedBorderColor);
+
+			circle.getProperty('radius').should.equal(specifiedRadius);
+		});
+
+		it('Should set only the properties passed in the constructor and use the default for the rest.', function() {
+			var specifiedBorderType = 'solid',
+				specifiedBorderColor = 'blue',
+				specifiedRadius = 32,
+				circle = new Circle({
+					border : {
+						type : specifiedBorderType,
+						color : specifiedBorderColor
+					},
+					radius: specifiedRadius,
+				});
+
+			circle.getProperty('position.x').should.equal(0);
+			circle.getProperty('position.y').should.equal(0);
+			circle.getProperty('position.z').should.equal(0);
+			circle.getProperty('opacity').should.equal(1);
+			circle.getProperty('border.type').should.equal(specifiedBorderType);
+			circle.getProperty('border.color').should.equal(specifiedBorderColor);
+
+			circle.getProperty('radius').should.equal(specifiedRadius);
+		});
+	});
+
+	describe('Serialization', function() {
+		it('toJSON should return json', function() { 
+			var rec = new Circle({'radius' : 200}),
+				json = rec.toJSON();
+
+			json.should.have.property('properties');
+			json.properties.should.have.property('position');
+			json.properties.should.have.property('border');
+			json.properties.should.have.property('fill');
+			json.properties.should.have.property('radius', 200);
+		});
+
+		it('fromJSON should load the object', function() { 
+			var rec = new Circle({'radius' : 200}),
+				json = rec.toJSON(),
+				rec2 = new Circle();
+
+			rec2.fromJSON(json);
+			rec2.getGuid().should.equal(rec.getGuid());
+			rec2.getProperties().should.have.property('radius', 200);
+		});
+	});	
+});
