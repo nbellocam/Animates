3ec3c46723a5050622a290cbc7752cb645e2d4ac
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-08-18 17:40:42 -0300

    Added support for images in the view

--- code/view/app/scripts/directives/property.js
+++ code/view/app/scripts/directives/property.js
@@ -1,7 +1,25 @@
 'use strict';
 
 angular.module('animatesApp')
-	.directive('animatesProperty', function () {
+	.directive('animatesProperty', function ($window, $q) {
+		var fileToDataURL = function (file) {
+				var deferred = $q.defer();
+				var reader = new FileReader();
+				reader.onload = function (e) {
+						deferred.resolve(e.target.result);
+					};
+				reader.readAsDataURL(file);
+				return deferred.promise;
+			},
+			processImageFile = function (file, scope) {
+				fileToDataURL(file).then(function (dataURL) {
+					if (dataURL) {
+						scope.prop.parse(dataURL);
+						scope.updatehandler( { 'key' : scope.propkey, 'value':  scope.prop.value() });
+					}
+				});
+			};
+
 		return {
 			restrict: 'E',
 			scope: {
@@ -11,7 +29,7 @@ angular.module('animatesApp')
 			},
 			template:
 				'<textarea ng-show="isText()" ng-rows="rows()" type="text" value="{{ prop.value() }}" id="property.{{ propkey }}" class="property form-control text" ng-blur="propertyBlur($event)"></textarea>' +
-				'<input ng-show="!isText()" type="" value="{{ prop.value() }}" id="property.{{ propkey }}" class="property form-control" ng-blur="propertyBlur($event)"></input>',
+				'<input ng-show="!isText()" type="" value="{{ getValue() }}" id="property.{{ propkey }}" class="property form-control" ng-blur="propertyBlur($event)"></input>',
 			controller: function($scope) {
 				$scope.isValid = true;
 				$scope.isText = function () { return $scope.prop.type().name() === 'text'; };
@@ -20,7 +38,7 @@ angular.module('animatesApp')
 				$scope.propertyBlur = function (event) {
 					var oldValue = $scope.prop.value(),
 						newValue = event.target.value;
-					
+
 					//TODO controller should not be accesing the dom
 					event.target.style.color = 'black';
 
@@ -33,6 +51,14 @@ angular.module('animatesApp')
 						}
 					}
 				};
+
+				$scope.getValue = function () {
+					if($scope.prop.type().name() === 'imageFile') {
+						return '';
+					}
+
+					return $scope.prop.value();
+				};
 			},
 			link : function (scope, element) {
 				var input;
@@ -44,14 +70,14 @@ angular.module('animatesApp')
 				}
 
 				scope.updateValue = function (value) {
-					input.val(value);
+					input.val((scope.prop.type().name() === 'imageFile') ? '' : value);
 				};
 
 				switch (scope.prop.type().name()) {
 					case 'color':
 						input.minicolors();
 						input.minicolors('value', scope.prop.value());
-						
+
 						scope.updateValue = function (newVal) {
 							input.minicolors('value', newVal);
 						};
@@ -61,6 +87,17 @@ angular.module('animatesApp')
 						break;
 					case 'float':
 						input.attr('type', 'number');
+						break;
+					case 'imageFile':
+						input.attr('type', 'file');
+						input.attr('accept','image/*');
+						input.on('change', function (evt) {
+							var files = evt.target.files;
+							for(var i = 0; i < files.length; i++) {
+								processImageFile(files[i], scope);
+							}
+						});
+
 						break;
 
 				}
