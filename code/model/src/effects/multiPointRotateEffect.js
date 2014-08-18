81e21e0ece6b65aac1bb26ebe2cc6ce0c4e48e1d
KidD32 <pbiasotti@hotmail.com>
2014-08-18 16:49:40 -0300

    Implemented counter-clockwise rotation

--- code/model/src/effects/multiPointRotateEffect.js
+++ code/model/src/effects/multiPointRotateEffect.js
@@ -49,8 +49,36 @@ function MultiPointRotateEffect(options, builder) {
 	}());
 
 	function getAngleFor(currentTick, startPoint, endPoint, clockwise) {
-		var m = (endPoint.angle - startPoint.angle) / (endPoint.tick - startPoint.tick);
-		return startPoint.angle + (m * currentTick);
+		var startTick = startPoint.tick,
+			endTick = endPoint.tick,
+			startAngle = startPoint.angle,
+			endAngle = endPoint.angle,
+			resultAngle,
+			newLoopTick;
+
+		function getLinearAngle (sAngle, eAngle, sTick, eTick, tick) {
+			return sAngle + (((eAngle - sAngle) / (eTick - sTick)) * (tick - startTick));
+		}
+
+		if (clockwise && (startAngle > endAngle)) {
+			// 359, 360, 1
+			resultAngle = getLinearAngle(startAngle, endAngle + 360, startTick, endTick, currentTick);
+
+			if (resultAngle > 360) {
+				resultAngle -= 360;	
+			}
+		} else if (!clockwise && (startAngle < endAngle)) {
+			// 1, 0, 359
+			resultAngle = getLinearAngle(startAngle + 360, endAngle, startTick, endTick, currentTick);
+
+			if (resultAngle >= 360) {
+				resultAngle -= 360;
+			}
+		} else {
+			resultAngle = getLinearAngle(startAngle, endAngle, startTick, endTick, currentTick);
+		}
+		
+		return resultAngle;
 	}
 
 	/**
@@ -66,10 +94,10 @@ function MultiPointRotateEffect(options, builder) {
 			if (!segment.endPoint) {
 				mediaFrameProperties.angle = segment.startPoint.angle;
 			} else {
-				mediaFrameProperties.angle = getAngleFor(tick, segment.startPoint, segment.endPoint, true);
+				mediaFrameProperties.angle = getAngleFor(tick, segment.startPoint, segment.endPoint, segment.endPoint.motion === 'clockwise');
 			}
-		} else if (points.length === 1 && tick >= points[0].tick) {
-			mediaFrameProperties.angle = points[0].angle;
+		} else if (segment && segment.endPoint) {
+			mediaFrameProperties.angle = segment.endPoint.angle;
 		}
 
 		return mediaFrameProperties;
