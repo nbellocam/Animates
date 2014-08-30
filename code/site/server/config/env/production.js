cdd600b0fb39153b2ebd66926b401da57ecde50c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-08-30 18:05:43 -0300

    Configured facebook for oauth login

--- code/site/server/config/env/production.js
+++ code/site/server/config/env/production.js
@@ -8,28 +8,28 @@ module.exports = {
          'mongodb://AnimatesMongoDB:Tj2h2YKxcrYT3LbrIQJbj4UftCrCwPWqsn4USqdVJ6M-@ds031088.mongolab.com:31088/AnimatesMongoDB'
   },
   facebook: {
-    clientID: "APP_ID",
-    clientSecret: "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    clientID: "706834929406764",
+    clientSecret: "55a89af5cfa7827fcf9d7b860700f70e",
+    callbackURL: "http://animates.azurewebsites.net/auth/facebook/callback"
   },
   twitter: {
     clientID: "CONSUMER_KEY",
     clientSecret: "CONSUMER_SECRET",
-    callbackURL: "http://localhost:3000/auth/twitter/callback"
+    callbackURL: "http://animates.azurewebsites.net/auth/twitter/callback"
   },
   github: {
     clientID: "APP_ID",
     clientSecret: "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    callbackURL: "http://animates.azurewebsites.net/auth/github/callback"
   },
   google: {
     clientID: "APP_ID",
     clientSecret: "APP_SECRET",
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "http://animates.azurewebsites.net/auth/google/callback"
   },
   linkedin: {
     clientID: "API_KEY",
     clientSecret: "SECRET_KEY",
-    callbackURL: "http://localhost:3000/auth/linkedin/callback"
+    callbackURL: "http://animates.azurewebsites.net/auth/linkedin/callback"
   }
 };
