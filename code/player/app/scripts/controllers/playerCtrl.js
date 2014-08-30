057ae2b0a84c741f132ca063de1ec43b27614409
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-08-30 16:17:13 -0300

    Removed editor controller and move all the logic to playerController

--- code/player/app/scripts/controllers/playerCtrl.js
+++ code/player/app/scripts/controllers/playerCtrl.js
@@ -1,12 +1,38 @@
 'use strict';
 
 angular.module('animatesApp')
-	.controller('PlayerCtrl', function ToolbarPanelCtrl($scope, animationService, presentationPlayerService) {
+	.controller('PlayerCtrl', function ToolbarPanelCtrl($scope, animationService, presentationPlayerService, canvasService, serverService, localAnimationStateService) {
+		$scope.loading = true;
+		$scope.playing = false;
+		$scope.errorMessage = undefined;
+
 		$scope.$watch(function() {
 				return animationService.isEditingEnable;
 			}, function() {
 				$scope.playing = !animationService.isEditingEnable;
 			});
+
+		function initializeLayout() {
+			angular.element(document).ready(function () {
+				angular.element('body').layout({
+					spacing: 0,
+					'north__paneSelector': '.outer-layout-north',
+					'center__paneSelector': '.outer-layout-center'
+				});
+
+				var innerLayout = angular.element('div.outer-layout-center').layout({
+					applyDefaultStyles: false,
+					south:{
+						size: 100,
+						onresize : function (panelName, element) {
+							var pane = angular.element(element);
+							angular.element('animates-timelines').css('height', pane.height() - (pane.outerHeight() - pane.height()) + 'px');
+						}
+					}
+				});
+				innerLayout.resizeAll();
+			});
+		}
 
 		// Other methods
 
@@ -28,5 +54,27 @@ angular.module('animatesApp')
 
 		$scope.stepBackward = function () {
 			presentationPlayerService.stepBackward(50);
+		};
+
+		$scope.$watch(function() {
+				return animationService.isEditingEnable;
+			}, function() {
+				$scope.playing = !animationService.isEditingEnable;
+			});
+
+		$scope.initializeAnimation = function (id) {
+			$scope.loading = true;
+
+			serverService.loadProject(id, function success(data) {
+					animationService.getInstance().loadProject(data.animation);
+					canvasService.createCanvas();
+					initializeLayout();
+					localAnimationStateService.setCurrentTick(0);
+					$scope.loading = false;
+				}, function error(data) {
+					console.log('Error: ' + data);
+					$scope.errorMessage = data || 'An error occurs.';
+					$scope.loading = false;
+				});
 		};
 	});
