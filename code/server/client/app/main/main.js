85363635a40eb5bf5d9b5b2a2e6463cb7af9bb97
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-01 21:21:52 -0300

    Added server generated using the latest angular-fullstack generator

---
+++ code/server/client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('animatesApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
