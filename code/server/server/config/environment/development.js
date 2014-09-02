310ec524b31c3a480e216a5866903a90d7ed8fd8
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-01 21:48:00 -0300

    Added dev credentials

--- code/server/server/config/environment/development.js
+++ code/server/server/config/environment/development.js
@@ -8,5 +8,23 @@ module.exports = {
     uri: 'mongodb://localhost/animates-dev'
   },
 
+  facebook: {
+    clientID:     '706836949406562',
+    clientSecret: '7ea17bdc211b7d48b77a28ec2600febe',
+    callbackURL:  'http://localhost:9000/auth/facebook/callback'
+  },
+
+  twitter: {
+    clientID:     process.env.TWITTER_ID || 'id',
+    clientSecret: process.env.TWITTER_SECRET || 'secret',
+    callbackURL:  (process.env.DOMAIN || '') + '/auth/twitter/callback'
+  },
+
+  google: {
+    clientID:     process.env.GOOGLE_ID || 'id',
+    clientSecret: process.env.GOOGLE_SECRET || 'secret',
+    callbackURL:  (process.env.DOMAIN || '') + '/auth/google/callback'
+  },
+
   seedDB: true
 };
