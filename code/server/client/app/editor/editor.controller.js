f88c20cba2261e163d4261f3f8a1fe95140e2b03
KidD32 <pbiasotti@hotmail.com>
2014-09-04 00:36:16 -0300

    Minor fix. socket io still not configured

--- code/server/client/app/editor/editor.controller.js
+++ code/server/client/app/editor/editor.controller.js
@@ -4,12 +4,7 @@ angular.module('animatesApp')
   .controller('EditorCtrl', function ($scope, $http, socket, $routeParams) {
     $scope.project = null;
 
-    function onSave (event, item) {
-       $scope.project.name = item.name;
-    }
-
     $http.get('/api/projects/' +  $routeParams.id).success(function(project) {
       $scope.project = project;
-      socket.syncUpdates('project', $scope.projects, onSave );
     });
   });
