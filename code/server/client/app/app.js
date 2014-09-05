1696b210f6ed607f4c0e5e4db939b3438bc4239c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-05 12:53:53 -0300

    Added animatesEditor dependency

--- code/server/client/app/app.js
+++ code/server/client/app/app.js
@@ -6,7 +6,8 @@ angular.module('animatesApp', [
   'ngSanitize',
   'ngRoute',
   'btford.socket-io',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'animatesEditor'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
