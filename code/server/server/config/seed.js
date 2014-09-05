8e4638b2db7c479d0bc19e89fd29ee00a5669d06
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-05 19:41:53 -0300

    Fixed issue with seed

--- code/server/server/config/seed.js
+++ code/server/server/config/seed.js
@@ -9,40 +9,38 @@ var User = require('../api/user/user.model');
 var Project = require('../api/project/project.model');
 
 User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, {
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, function(err, newUser) {
-      if (err){
-        console.log('An error occurs while creating the users');
-        return;
-      }
+    User.create({
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@admin.com',
+        password: 'admin'
+    }, {
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@test.com',
+        password: 'test'
+    }, function(err, adminUser, testUser) {
+        if (err) {
+            console.log('An error occurs while creating the users');
+            return;
+        }
 
-      console.log('finished populating users');
+        console.log('finished populating users');
 
-      //Clear old projects, then add projects in
-      Project.find({}).remove(function() {
+        //Clear old projects, then add projects in
+        Project.find({}).remove(function() {
         Project.create({
-          name: 'Animation test',
-          info: 'This is an incredible animation test.',
-          user: newUser._id
+            name: 'Animation test',
+            info: 'This is an incredible animation test.',
+            user: testUser._id
         }, {
-          name: 'Animation: The movie',
-          info: 'You should see the second part, it is incredible.',
-          user: newUser._id
-        }, function() {
-             console.log('finished populating projects');
-          }
-        );
-      });
-    }
-  );
+            name: 'Animation: The movie',
+            info: 'You should see the second part, it is incredible.',
+            user: testUser._id
+            }, function() {
+                console.log('finished populating projects');
+            });
+        });
+    });
 });
