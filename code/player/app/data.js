08da884f9eb87af61bd89d3b9d6655bb40c64177
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-07 23:30:10 -0300

    Updated player to load the data using the script approach

---
+++ code/player/app/data.js
@@ -0,0 +1,146 @@
+var animationData = {
+  "animation": {
+        "type": "Animation",
+        "data": {
+          "canvas": {
+            "type": "Canvas",
+            "data": {
+              "height": 400,
+              "width": 600,
+              "backgroundColor": "white",
+              "backgroundImage": ""
+            }
+          },
+          "timeline": {
+            "type": "Timeline",
+            "data": {
+              "mediaTimelines": [{
+                "type": "MediaTimeline",
+                "data": {
+                  "mediaObject": {
+                    "type": "Rectangle",
+                    "data": {
+                      "properties": {
+                        "height": 100,
+                        "width": 100,
+                        "opacity": 1,
+                        "angle": 0,
+                        "fill": "#40f490",
+                        "position": {
+                          "x": 150,
+                          "y": 150,
+                          "z": 0
+                        },
+                        "border": {
+                          "type": "none",
+                          "width": 0,
+                          "color": "#000000"
+                        },
+                        "name": "Test rectangle"
+                      },
+                      "guid": "0993799b-2bc9-4d69-b031-cd4f6913e2f1"
+                    }
+                  },
+                  "effects": {
+                    "d6a1c2f2-1125-4d10-8f43-f1d173394105": {
+                      "type": "MultiPointMoveEffect",
+                      "data": {
+                        "options": {
+                          "path": "Straight",
+                          "points": {
+                            "12cac05a-2d48-4b11-bb09-65cb5d92cb90": {
+                              "position": {
+                                "x": 150,
+                                "y": 150
+                              },
+                              "tick": 0
+                            },
+                            "81436050-4c2d-4674-a748-1f34324b9fec": {
+                              "position": {
+                                "x": 550,
+                                "y": 0
+                              },
+                              "tick": 200
+                            },
+                            "8e35a9d0-ad1f-4415-bd56-6525a5cad5d1": {
+                              "position": {
+                                "x": 350,
+                                "y": 550
+                              },
+                              "tick": 350
+                            }
+                          }
+                        },
+                        "guid": "d6a1c2f2-1125-4d10-8f43-f1d173394105"
+                      }
+                    },
+                    "c3cd2515-9a48-4766-a6c0-8390d1445b4a": {
+                      "type": "MultiPointRotateEffect",
+                      "data": {
+                        "options": {
+                          "points": {
+                            "1e9f6cc9-91e3-41f0-af03-ad6c3ff12b7e": {
+                              "angle": 0,
+                              "motion": "clockwise",
+                              "tick": 0
+                            },
+                            "8591aa30-2a1a-4158-8424-194bb8cdf56c": {
+                              "angle": 30,
+                              "motion": "clockwise",
+                              "tick": 200
+                            },
+                            "264a975c-4714-4697-a5a4-171a86e81d22": {
+                              "angle": 80,
+                              "motion": "clockwise",
+                              "tick": 300
+                            }
+                          }
+                        },
+                        "guid": "c3cd2515-9a48-4766-a6c0-8390d1445b4a"
+                      }
+                    },
+                    "194b51de-02ff-4a74-a6ce-40053f52d305": {
+                      "type": "MultiPointWidthAndHeightScaleEffect",
+                      "data": {
+                        "options": {
+                          "points": {
+                            "f800e9cb-7d95-4658-9361-392bca660710": {
+                              "height": 100,
+                              "width": 100,
+                              "tick": 0
+                            },
+                            "e8ab0c31-6f58-43b7-8c53-4a6d679bf5a2": {
+                              "height": 100,
+                              "width": 230,
+                              "tick": 200
+                            },
+                            "2cf47d9e-6022-4e96-8c4d-d5168d804d60": {
+                              "height": 430,
+                              "width": 230,
+                              "tick": 300
+                            }
+                          }
+                        },
+                        "guid": "194b51de-02ff-4a74-a6ce-40053f52d305"
+                      }
+                    },
+                    "9355e717-0beb-403c-9614-b118f6014292": {
+                      "type": "FadeEffect",
+                      "data": {
+                        "options": {
+                          "startOpacity": 0,
+                          "endOpacity": 1,
+                          "endTick": 100,
+                          "startTick": 0
+                        },
+                        "guid": "9355e717-0beb-403c-9614-b118f6014292"
+                      }
+                    }
+                  }
+                }
+              }]
+            }
+          }
+        }
+      }
+  };
--- code/player/app/scripts/services/serverService.js
+++ code/player/app/scripts/services/serverService.js
@@ -2,17 +2,41 @@
 
 angular.module('animatesPlayer')
 	.service('playerServerService', function playerServerService($http, $window, $timeout) {
+
+		function loadData(success, error) {
+			var timeout = 100; // 10 seconds timeout
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = 'data.js';
+			document.body.appendChild(script);
+
+			function poll() {
+			  $timeout(function () {
+			    timeout--;
+			    if (typeof $window.animationData !== 'undefined') {
+			      success($window.animationData);
+			    }
+			    else if (timeout > 0) {
+			      poll();
+			    }
+			    else {
+			      error();
+			    }
+			  }, 100);
+			}
+
+			poll();
+		}
+
 		this.loadProject = function (id, success, error) {
-			$http.get('./data.json')
-				.success(function (data) {
-					$timeout(function() {
-						success(data);
-					}, 600);
-				})
-				.error(function () {
-					$http.get('/api/projects/' + encodeURIComponent(id))
-						.success(success)
-						.error(error);
-				});
+			if (id === 'projectId') {
+				loadData(success, error);
+			} else if (id !== undefined) {
+				$http.get('/api/projects/' + encodeURIComponent(id))
+					.success(success)
+					.error(error);
+			} else {
+				error();
+			}
 		};
 	});
