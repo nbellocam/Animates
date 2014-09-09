adc53bc63f2a8b0089772e9a52a645e52626402c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-09 16:00:04 -0300

    Removed old site project

--- code/site/app/scripts/services/localStorageConstants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('animatesApp')
-  .constant('localStorageConstants', {
-    recentProjects : 'recentProjects',
-    maxRecentProjects : 15
-  });
--- code/site/app/scripts/services/session.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('animatesApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
