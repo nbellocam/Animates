31fd45d8ed38e571a79ab8b27117669df2514e66
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-09 16:41:58 -0300

    Updated editor nav bar menus to show share, download and play options

--- code/server/client/app/editor/editor.controller.js
+++ code/server/client/app/editor/editor.controller.js
@@ -4,6 +4,7 @@ angular.module('animatesApp')
   .controller('EditorViewCtrl', function ($scope, $http, socket, $routeParams, Modal) {
     $scope.project = undefined;
     $scope.socket = socket.socket;
+    $scope.playUrl = '';
 
     $scope.titleChange = function (newValue) {
       var jsonPatch = { name : newValue };
@@ -22,9 +23,32 @@ angular.module('animatesApp')
       });
     };
 
+    $scope.shareProject = function() {
+        var modal = Modal.form.share(angular.noop, $scope.project._id);
+        modal();
+    };
+
+    $scope.downloadProject = function() {
+        $http.get('/api/projects/' + $scope.project._id + '/download', {responseType: 'arraybuffer'})
+          .success(function(projectZip) {
+            var hiddenElement = document.createElement('a');
+            hiddenElement.setAttribute('id', 'hiddenDownloadElement');
+            var blob = new Blob([projectZip], {type: 'application/zip'});
+            hiddenElement.href = window.URL.createObjectURL(blob);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'animation.zip';
+            hiddenElement.click();
+          })
+          .error(function () {
+            var errorModal = Modal.alerts.error();
+            errorModal();
+          });
+    };
+
     $http.get('/api/projects/' +  $routeParams.id).success(function(project) {
       project.id = project._id;
       $scope.project = project;
+      $scope.playUrl = '/projects/' + project._id + '/play';
       $scope.$broadcast('projectLoaded', project);
     })
     .error(function() {
