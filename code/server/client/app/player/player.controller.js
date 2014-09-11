33e29b5bd359d8fd7223328c155352cd9ee735f7
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-09-11 02:42:35 -0300

    Fixed download feature in player

--- code/server/client/app/player/player.controller.js
+++ code/server/client/app/player/player.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('animatesApp')
-  .controller('PlayerViewCtrl', function ($scope, $http, socket, $routeParams) {
+  .controller('PlayerViewCtrl', function ($scope, $http, socket, $routeParams, Modal) {
     $scope.project = undefined;
     $scope.projectId = undefined;
 
@@ -11,4 +11,21 @@ angular.module('animatesApp')
       $scope.projectId = project._id;
       $scope.$broadcast('projectLoaded', project);
     });
+
+    $scope.downloadProject = function() {
+        $http.get('/api/projects/' + $scope.project._id + '/download', {responseType: 'arraybuffer'})
+          .success(function(projectZip) {
+            var hiddenElement = document.createElement('a');
+            hiddenElement.setAttribute('id', 'hiddenDownloadElement');
+            var blob = new Blob([projectZip], {type: 'application/zip'});
+            hiddenElement.href = window.URL.createObjectURL(blob);
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'animation.zip';
+            hiddenElement.click();
+          })
+          .error(function () {
+            var errorModal = Modal.alerts.error();
+            errorModal();
+          });
+    };
   });
